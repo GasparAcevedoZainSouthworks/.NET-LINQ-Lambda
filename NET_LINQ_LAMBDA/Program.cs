27e4e9fb9826aa6ee4c6f4cc8da0a9b94f24d230
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-10-08 16:32:16 -0300

    Project & .gitignore files were added

---
+++ NET_LINQ_LAMBDA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET_LINQ_LAMBDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
